1dd88b5a33cd0107bc95be78df2deca19ae4bdb4
ByJuix <julian@toast24.de>
2024-07-18 16:43:18 +0200

    changed head

--- index.php
+++ index.php
@@ -40,11 +40,12 @@
 <!DOCTYPE html>
 <html lang="de">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portflow</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Portflow</title>
+  <link rel="icon" href="./includes/img/portflow.ico" type="image/x-icon">
+  <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="flex flex-col items-center justify-center h-screen bg-gray-200">
   <div class="bg-white shadow-lg rounded-2xl p-12 w-full max-w-lg">
